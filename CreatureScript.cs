de882fd03c54337afac2f90034fc672cde485e04
Tom Selfin <selfint@gmail.com>
2019-09-29 22:09:33 +0300

    Add health and energy to creatures
    Creatures should:
    - die when they run out of health
    - recover health when they have energy
    - gain energy when they eat food

--- CreatureScript.cs
+++ CreatureScript.cs
@@ -8,18 +8,39 @@ public class CreatureScript : MonoBehaviour {
     public GameObject mouth;
     public GameObject womb;
     public GameObject[] blocks;
+    public float initialHealth = 100f;
+    public float initialEnergy = 100f;
+    public float health, energy;
+    public bool dead = false;
 
     void Start() {
-
         // set the creature mass to be the sum of its blocks mass
         rb.mass = 20 + blocks.Length * 10;
+        this.health = initialHealth;
+        this.energy = initialEnergy;
     }
 
     void FixedUpdate() {
-    
+        // decrease energy as long as creature is alive
+        this.energy = Mathf.Max(0, this.energy - 1);
+
+        // if energy is 0 decrease the creature's health
+        if (this.energy <= 0) {
+            this.health -= 1;
+        }
+
+        // if the creature has energy, regain health up to initial health
+        else {
+            this.health = Mathf.Min(initialHealth, this.health + 1);
+        }
+
+        // if health is 0 kill this creature
+        if (this.health <= 0)
+            this.dead = true;
     }
 
     public void EatFood(GameObject food) {
+        this.energy += 1;
         Destroy(food);
     }
 
