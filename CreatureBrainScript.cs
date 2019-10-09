9a0cba6751016e6e265aa618130c4313def505c7
Tom Selfin <selfint@gmail.com>
2019-10-09 13:39:20 +0300

    Implement CreatureBrainScript as a neural network
    Neuron class was unnecassary, implementing the activation function
    and keeping track of the biases in the CreatureBrainScript class is
    sufficient.

--- CreatureBrainScript.cs
+++ CreatureBrainScript.cs
@@ -2,22 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatureBrainScript
-{
-    public Neuron[] neurons;
-    public float[] outputs;
+public class CreatureBrainScript {
+    public int inputAmount, outputAmount;
+    public float[][] weights;
+    public float[] biases;
 
-    public virtual void ReceiveSignal(List<float>[] inputSignals) {
-        for (int i = 0; i < this.neurons.Length; i++) {
-            this.neurons[i].SendOutput(inputSignals[i]);
+    public CreatureBrainScript() {
+
+    }
+
+    public CreatureBrainScript(int inputAmount, int outputAmount) {
+        this.inputAmount = inputAmount;
+        this.outputAmount = outputAmount;
+
+        // generate random initial weights
+        this.weights = new float[inputAmount][];
+        for (int i = 0; i < inputAmount; i++) {
+            this.weights[i] = new float[outputAmount];
+            for (int j = 0; j < outputAmount; j++) {
+                this.weights[i][j] = Random.Range(-1f, 1f);
+            }
+        }
+
+        // generate random initial biases
+        this.biases = new float[outputAmount];
+        for (int i = 0; i < outputAmount; i++) {
+            this.biases[i] = Random.Range(-1f, 1f);
         }
     }
 
-    public virtual void Action() {
+    public virtual float[] FeedForward(float[] inputSignals) {
+        float[] brainOutput = new float[outputAmount];
+        for (int j = 0; j < outputAmount; j++) {
+            float neuronInput = 0f;
+            for (int i = 0; i < inputAmount; i++) {
+                neuronInput += inputSignals[i] * this.weights[i][j];
+            }
+            brainOutput[j] = Sigmoid(neuronInput) + this.biases[j];
+        }
 
+        return brainOutput;
     }
 
-    public virtual void UnconsciousAction() {
+    public static float Sigmoid(float x) {
+        return 1f / (1f - Mathf.Exp(-x));
+    }
 
+    public static float ReLu(float x) {
+        return Mathf.Max(0, x);
     }
 }
