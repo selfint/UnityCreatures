76c25da3743387c4729c46254389b14d1b33c353
Tom Selfin <selfint@gmail.com>
2019-10-09 14:42:37 +0300

    Implement basic neural activity

--- CreatureScript.cs
+++ CreatureScript.cs
@@ -8,6 +8,7 @@ public class CreatureScript : MonoBehaviour {
     public List<GameObject> blocks;
     [System.NonSerialized]
     public CreatureBrainScript brain;
+    public GameObject[] inputBlocks, outputBlocks;
     public float initialHealth, initialEnergy;
     public float maxHealth, maxEnergy;
     public float health, energy;
@@ -34,18 +35,19 @@ public class CreatureScript : MonoBehaviour {
 
         // map the children of this objects as a list
         // find how many inputs (sensors) and outputs (actions) the creature has
-        int inputs = 0, outputs = 0;
+        List<GameObject> inputs = new List<GameObject>(), outputs = new List<GameObject>();
         for (int i = 0; i < transform.childCount; i++) {
             GameObject block = transform.GetChild(i).gameObject;
             if (block.tag == "ScentSensor") {
-                inputs += 1;
+                inputs.Add(block);
             } else if (block.tag == "Motor" || block.tag == "Womb") {
-                outputs += 1;
+                outputs.Add(block);
             }
             this.blocks.Add(block);
         }
-
-        this.brain = new CreatureBrainScript(inputs, outputs);
+        this.inputBlocks = inputs.ToArray();
+        this.outputBlocks = outputs.ToArray();
+        this.brain = new CreatureBrainScript(inputBlocks.Length, outputBlocks.Length);
     }
 
     void FixedUpdate() {
@@ -55,23 +57,29 @@ public class CreatureScript : MonoBehaviour {
     }
 
     private void ManageReproduction() {
-        // only birth one child at a time
-        if (!this.reproduce)
-            this.reproductionWill += matingRate;
-
         // if reproduction will is high enough, spawn a new child
         if (this.reproductionWill >= this.reproductionThreshold) {
             this.reproductionWill = 0f;
 
-            // GameManager will take care of generating the new child
+            // GameManager will set this to false when it creates the child
             this.reproduce = true;
         }
     }
 
     public void ManageBlocks() {
-        foreach (GameObject block in this.blocks) {
-            if (block.tag == "ScentSensor") {
-                ManageScentSensor(block, 100);
+        float[] inputs = new float[inputBlocks.Length];
+        for (int i = 0; i < inputBlocks.Length; i++) {
+            if (this.inputBlocks[i].tag == "ScentSensor") {
+                inputs[i] = ManageScentSensor(this.inputBlocks[i], 100);
+            }
+        }
+
+        float[] outputs = this.brain.FeedForward(inputs);
+        for (int i = 0; i < outputs.Length; i++) {
+            if (this.outputBlocks[i].tag == "Motor") {
+                rb.AddForce(this.outputBlocks[i].transform.forward * outputs[i] * Time.deltaTime * 10, ForceMode.Force);
+            } else if (this.outputBlocks[i].tag == "Womb") {
+                this.reproductionWill += matingRate * outputs[i];
             }
         }
     }
