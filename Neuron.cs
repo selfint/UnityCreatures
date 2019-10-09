61197aef49fe7a1d98744910ac40ba18ca7ef7a9
Tom Selfin <selfint@gmail.com>
2019-10-09 13:03:43 +0300

    Begin implementing neural activity in creatures
    The womb will act as the 'brain' of the creature, with the sensor blocks
    providing the inputs and the actions blocks being the outputs.

---
+++ Neuron.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neuron {
+    public List<float> inputs;
+    public float bias, output;
+
+    public Neuron() {
+        this.inputs = new List<float>();
+        this.bias = Random.Range(-1f, 1f);
+    }
+
+    public static float Sigmoid(float x) {
+        return 1f / (1f - Mathf.Exp(-x));
+    }
+
+    public static float ReLu(float x) {
+        return Mathf.Max(0, x);
+    }
+
+    public float SendOutput(List<float> inputSignals) {
+        this.inputs = inputSignals;
+        float inputSum = 0f;
+        foreach (float input in inputSignals) {
+            inputSum += input;
+        }
+        this.output = ReLu(inputSum) + this.bias;
+        return this.output;
+    }
+
+
+}
---
+++ WombBlockScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WombBlockScript : MonoBehaviour {
+    public bool mutationBlock;
+    public bool dynamicCostBlock;
+    public float cost;
+    public Neuron[] neurons;
+    public float[] outputs;
+
+    public virtual void ReceiveSignal(List<float>[] inputSignals) {
+        for (int i = 0; i < this.neurons.Length; i++) {
+            this.neurons[i].SendOutput(inputSignals[i]);
+        }
+    }
+
+    public virtual void Action() {
+
+    }
+
+    public virtual void UnconsciousAction() {
+
+    }
+
+}
